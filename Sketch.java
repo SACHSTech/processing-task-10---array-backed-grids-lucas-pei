84b13095c859ad1d4f5a89527856cec2de75c882
lucas-pei <lucaspei39@gmail.com>
2022-05-18 09:36:36 -0400

    format

--- Sketch.java
+++ Sketch.java
@@ -2,7 +2,7 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 
-	// global variables
+  // global variables
   int intCellWidth = 20;
   int intCellHeight = 20;
   int intMargin = 5;
